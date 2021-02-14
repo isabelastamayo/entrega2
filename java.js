b3bc8e8414762e23cdf192fe94243deddcb53e44
isabelastamayo <77944069+isabelastamayo@users.noreply.github.com>
2021-02-14 18:42:30 -0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,39 @@
+var vec=["juan","lola","lina","sofia"];
+console.log(vec);
+
+var si=function()
+{
+	var i=0;
+	var nom=document.getElementById("nom").value;
+	var ul=vec.length-1; 
+	while(i<vec.length)
+	{
+		if(nom==vec[i])
+		{
+			var ult=vec[ul];
+			vec[ul]=vec[i];
+			vec[i]=ult;
+			vec.pop();
+			console.log(vec);
+			i=vec.length;
+			document.getElementById("re").value="el nombre fue eliminado";
+			document.getElementById("nom").value="";
+		}
+		else
+		{
+			i=i+1;
+			if (nom!=vec[i])
+			{
+				document.getElementById("re").value=nom+" no está en el vector";
+			}
+		}
+	}
+}
+
+var ing=function()
+{
+	var nombi=document.getElementById("nombi").value;
+	vec.push(nombi);
+	console.log(vec);
+	document.getElementById("nombi").value="";
+}
